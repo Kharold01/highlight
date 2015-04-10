82fa26489d9aadbcfae2274461fad84a1f4e4a58
Katie Harold <kharold@sjccs142.saintjoe.edu>
2015-04-10 11:47:39 -0400

    // added comments

--- js/highlightCode.js
+++ js/highlightCode.js
@@ -1,16 +1,24 @@
+// creates function with 2 parameters
 function highlightCode(node, keywords) {
+  // creates text variable to be all the text in the node	
   var text = node.textContent;
-  node.textContent = ""; // Clear the node
-
+  // clears the node by setting it to an empty string
+  node.textContent = ""; 
+  // creates variable match to loop over all the matches
   var match, pos = 0;
   while (match = keywords.exec(text)) {
+    // this is where  
     var before = text.slice(pos, match.index);
+    // the text in between gets appended as regular text nodes
     node.appendChild(document.createTextNode(before));
+    // this is where the matched text gets "highlighted"
     var strong = document.createElement("strong");
+    // this is also where the matched text gets "highlighted"
     strong.appendChild(document.createTextNode(match[0]));
     node.appendChild(strong);
     pos = keywords.lastIndex;
   }
+  // the text after gets made into a text node
   var after = text.slice(pos);
   node.appendChild(document.createTextNode(after));
 }
